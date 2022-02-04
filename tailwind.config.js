4a085c83344570159919d3bd3daf3ef61820d547
Annika Flyxe <aflyxe@gmail.com>
2022-02-04 01:25:53 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Lato: "'Lato', serif",
+      },
+
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
